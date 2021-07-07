957972c08e54f0e20ff3fbef76baff2ef217d68f
SebGenthon <sebastien@e-corses.com>
2021-07-07 18:21:47 +0200

    first

---
+++ public/js/zone/portail.js
@@ -0,0 +1,13 @@
+$(".first_website").on('mouseover',function(){
+    $(".vert").removeClass('hidden')
+})
+$(".first_website").on('mouseout',function(){
+    $(".vert").addClass('hidden')
+})
+
+$(".second_website").on('mouseover',function(){
+    $(".orange").removeClass('hidden')
+})
+$(".second_website").on('mouseout',function(){
+    $(".orange").addClass('hidden')
+})
